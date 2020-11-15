99e0d1c293d0e32ab949727dc7032cd40ac2e16b
Aoife O'Rourke <aoife.orourke@hotmail.com>
2020-11-15 17:36:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+# #program to simulate a fruit machine!
+# ## Brings in a random number generator
+# import random
+
+# #initialise the list of fruits - 5 elements
+# ##create a list called fruits
+# ###Lists use [], need , between each element, strings need "", numbers don't
+# fruits = ["Strawberry", "lemon", "Orange", "Raspberry", "Cherry"]
+
+# ##To display the list I use print()
+# print(fruits)
+
+# #generate three random numbers between 0 and 4 inclusive
+# ##create variables that choose random fruits
+# ##Fruit positions  are called indexes - start at 0
+# ##randint - chooses a random integer Value
+# ####(0,4) random integer will be between 0 and 4
+
+# selection1 = random.randint(0,4)
+# selection2 = random.randint(0,4)
+# selection3 = random.randint(0,4)
+
+# #show the results - display the fruits
+# ##i want to print a random selction from the fruits list
+# ###I have to print the list and call the variable ###print([])
+# print(fruits[selection1])
+# print(fruits[selection2])
+# print(fruits[selection3])
+# print("\n")
+
+
+
+# ####################################
+# choice1 = random.choice(fruits)
+# choice2 = random.choice(fruits)
+# choice3 = random.choice(fruits)
+# print(choice1)
+# print(choice2)
+# print(choice3)
+#######################################
+
+# fruits = ["strawberry", "lemon", "orange", "raspberry","cherry"]
+
+# print(fruits)
+# fruits[0] = "Apple"
+# fruit ="melon"
+
+# fruits[1] = fruit
+# fruits[2] = "raspberry"
+# fruits[3] = fruits[4]
+# fruits[4] = "pineapple"
+# print(fruits)
+#######################################
+
+
+##########################################
+
+
+
